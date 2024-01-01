a4b41e69124f7cc4567402355267d144eb5bd66c
Min Jiwon <minjiwon@Mins-MacBook-Pro.local>
2024-01-01 17:19:42 +0900

    chore: Add webpack loader for Linaria

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: {
+    rules: [
+      {
+        test: /\.js/,
+        use: [{ loader: "@wyw-in-js/webpack-loader" }],
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
