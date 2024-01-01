d402c3f156c5862947c93e77e6c5d7510622d27a
Min Jiwon <minjiwon@Mins-MacBook-Pro.local>
2024-01-01 18:08:36 +0900

    chore: Change jest config & setup as commonJS modules

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
-import nextJest from "next/jest";
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  dir: "./",
+	dir: './',
 });
 
-export default createJestConfig({
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    // '^@components/(.*)$': '<rootDir>/src/components/$1',
-    // '^@pages/(.*)$': '<rootDir>/src/pages/$1',
-    // '^@styles/(.*)$': '<rootDir>/src/styles/$1',
-    // '^@public/(.*)$': '<rootDir>/public/$1',
-  },
-  testEnvironment: "jest-environment-jsdom",
+module.exports = createJestConfig({
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/src/$1',
+		// '^@components/(.*)$': '<rootDir>/src/components/$1',
+		// '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+		// '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+		// '^@public/(.*)$': '<rootDir>/public/$1',
+	},
+	testEnvironment: 'jest-environment-jsdom',
 });
