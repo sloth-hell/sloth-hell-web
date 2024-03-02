1f6b7c291958978a883e6f265b16274dda4654d4
Min Jiwon <minjiwon@Mins-MacBook-Pro.local>
2024-03-02 16:12:24 +0900

    feat: Define additional color constnts

--- src/styles/themes.css.ts
+++ src/styles/themes.css.ts
@@ -19,18 +19,26 @@ export const _colors = {
 		black: '#1C1C1C',
 	},
 	primary: {
-		red: '#FB495E',
-		orange: '#FF8856',
-		brown: '#342224',
+		red: _tokenize(['#FB495E', '#693A40']),
+		orange: _tokenize(['#FF8856']),
+		brown: _tokenize(['#342224']),
 	},
-	gradient: {
-		'01': 'linear-gradient(135deg, #FB495E 0%, #FF8856 100%)',
-	},
+	gradient: _tokenize([
+		'linear-gradient(135deg, #FB495E 0%, #FF8856 100%)',
+		'linear-gradient(135deg, #1E1E1E 0%, #342224 100%)',
+	]),
 	gray: _tokenize(['#F1F1F1', '#C0C0C0', '#989898', '#646464', '#353535']),
-	line: _tokenize(['#747474']),
+	line: {
+		..._tokenize(['#747474', '#4F4F4F', '#353535']),
+		gradient: _tokenize(['linear-gradient(135deg, #84474D 0%, #413027 100%']),
+	},
 	system: {
 		error: '#E53030',
 		complete: '#2EB05A',
+	},
+	ui: {
+		toastpopup: 'rgba(#684448, 0.9)',
+		dim: 'rgba(0, 0, 0, 0.5)',
 	},
 };
 
