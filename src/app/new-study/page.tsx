a6c305f14e62bd58bde2c895e73d9e94898fe90b
Min Jiwon <minjiwon@Mins-MacBook-Pro.local>
2024-03-03 14:58:31 +0900

    feat: Action on BtnToggle

--- src/app/new-study/page.tsx
+++ src/app/new-study/page.tsx
@@ -1,14 +1,26 @@
+'use client';
+
+import { useState } from 'react';
 import DateTimeWheels from '@/components/DateTimeWheels';
 import { BtnConfirm, BtnToggle } from '@/components/_common/buttons';
 import styles from './styles.css';
 
 export default function NewStudyPage() {
+	const [active, setActive] = useState(true);
+	const handleClick = () => {
+		setActive(!active);
+	};
 	return (
 		<main className={styles.main}>
 			<DateTimeWheels />
 			<BtnConfirm title="확인" />
 			<div style={{ display: 'flex' }}>
-				<BtnToggle title="True" active={true} className={styles.test} />
+				<BtnToggle
+					title="True"
+					active={active}
+					onClick={handleClick}
+					className={styles.test}
+				/>
 				<BtnToggle title="False" active={false} className={styles.test} />
 			</div>
 		</main>
--- src/components/_common/buttons/BtnToggle/BtnToggle.tsx
+++ src/components/_common/buttons/BtnToggle/BtnToggle.tsx
@@ -1,26 +1,38 @@
+'use client';
+
+import type { RefObject, MouseEventHandler, MouseEvent } from 'react';
 import clx from 'classnames';
 import styles from './styles.css';
 
 interface BtnToggleProps {
 	title: string;
 	imgSrc?: string;
-	active?: Boolean;
+	active: boolean;
+	onClick?: MouseEventHandler<HTMLButtonElement>;
 	className?: string;
+	ref?: RefObject<HTMLButtonElement>;
 }
 
 export default function BtnToggle({
 	title,
 	imgSrc,
 	active,
+	onClick,
 	className,
+	ref,
 }: BtnToggleProps) {
+	const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+		if (onClick) {
+			onClick(event);
+		}
+	};
+
 	return (
 		<button
 			type="button"
-			className={clx(
-				styles.btnToggle({ active: (active ?? false) as boolean }),
-				className,
-			)}>
+			className={clx(styles.btnToggle({ active }), className)}
+			onClick={handleClick}
+			ref={ref}>
 			{imgSrc && (
 				<picture className={styles.image}>
 					<source srcSet={imgSrc} type="image/webp" />
