532d5f0ae76625fcfd9209892272c22006bfed81
Min Jiwon <minjiwon@Mins-MacBook-Pro.local>
2024-03-17 18:31:40 +0900

    feat: Page header

---
+++ src/components/_common/PageHeader/PageHeader.tsx
@@ -0,0 +1,39 @@
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+import styles, { vars } from './styles.css';
+
+export interface PageHeaderProps {
+	title?: string;
+	onBack?: () => void;
+	children?: React.ReactNode;
+	progress?: number;
+}
+
+export default function PageHeader({
+	title,
+	onBack,
+	children,
+	progress,
+}: PageHeaderProps) {
+	return (
+		<header className={styles.container}>
+			<div className={styles.body}>
+				<div className={styles.bodyLeft}>
+					{onBack && (
+						<button className={styles.back} onClick={onBack}>
+							<img src="/icons/arrow-left.svg" className={styles.backIcon} />
+						</button>
+					)}
+					<h1 className={styles.title}>{title}</h1>
+				</div>
+				{children}
+			</div>
+			{typeof progress === 'number' && (
+				<div className={styles.progressBar}>
+					<div
+						className={styles.progressBarActive}
+						style={assignInlineVars({ [vars.progressPercent]: `${progress}%` })}></div>
+				</div>
+			)}
+		</header>
+	);
+}
---
+++ src/components/_common/PageHeader/styles.css.ts
@@ -0,0 +1,74 @@
+import { style, createVar } from '@vanilla-extract/css';
+import { colors } from '@/styles/constants';
+
+export const vars = {
+	progressPercent: createVar(),
+};
+
+const container = style({
+	position: 'fixed',
+	width: '100%',
+	height: 'auto',
+	top: 0,
+	left: 0,
+	background: colors.bg['01'],
+});
+
+const body = style({
+	width: '100%',
+	height: 56,
+	margin: '0 16px',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'space-between',
+});
+
+const bodyLeft = style({
+	height: 26,
+	display: 'flex',
+	alignItems: 'center',
+});
+
+const back = style({
+	width: 24,
+	height: 24,
+	marginRight: 4,
+});
+
+const backIcon = style({
+	width: 24,
+	height: 24,
+});
+
+const title = style({
+	fontSize: 18,
+	color: colors.basic.white,
+});
+
+const progressBar = style({
+	width: '100%',
+	height: 4,
+	background: colors.line['02'],
+});
+
+const progressBarActive = style({
+	width: vars.progressPercent,
+	height: '100%',
+	willChange: 'width',
+	transition: 'width 150ms',
+	borderRadius: 2,
+	background: colors.gradient['01'],
+});
+
+const styles = {
+	container,
+	body,
+	bodyLeft,
+	back,
+	backIcon,
+	title,
+	progressBar,
+	progressBarActive,
+};
+
+export default styles;
