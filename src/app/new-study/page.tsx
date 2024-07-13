f79623bb88f4138e0458c471e1e34f46ae7ac33a
Min Jiwon <peace.cheejecake@gmail.com>
2024-07-13 18:28:22 +0900

    refactor: Separate module for new study creation

--- src/app/new-study/page.tsx
+++ src/app/new-study/page.tsx
@@ -1,14 +1,5 @@
-'use client';
+import StudyEditorModule from '@/components/modules/StudyEditorModule';
 
-import { useEffect, useState, useCallback } from 'react';
-import DateTimeWheels from '@/components/DateTimeWheels';
-import PageHeader from '@/components/_common/PageHeader';
-import { BtnConfirm, BtnToggle, IconBtn } from '@/components/_common/buttons';
-import { PopupBottom, PopupConfirm } from '@/components/_common/popups';
-import { InputTextSingle, InputTextRange, CheckBox } from '@/components/_common/inputs';
-import styles from './styles.css';
-import Tooltip from '@/components/_common/Tooltip';
-import Dropdown from '@/components/_common/Dropdown';
-import RadioButtonGroup from '@/components/_common/inputs/RadioButtonGroup';
-
-export default function NewStudyPage() {}
+export default function NewStudyPage() {
+	return <StudyEditorModule />;
+}
--- src/app/new-study/styles.css.ts
+++ src/app/new-study/styles.css.ts
@@ -1,32 +1,5 @@
 import { style } from '@vanilla-extract/css';
 
-export const main = style({
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-	justifyContent: 'center',
-	padding: '0 16px',
-	minHeight: '100vh',
-});
-
-export const description = style({
-	display: 'inherit',
-	justifyContent: 'center',
-	alignItems: 'center',
-	width: '100%',
-	zIndex: 2,
-});
-
-const test = style({
-	margin: 12,
-});
-
-const testPopup = style({});
-
-const styles = {
-	main,
-	test,
-	testPopup,
-};
+const styles = {};
 
 export default styles;
