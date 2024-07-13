180565a833d231340b80a342bd8bd1b229ca93dc
Min Jiwon <peace.cheejecake@gmail.com>
2024-07-13 18:01:18 +0900

    chore: Create common index file for common all components

---
+++ src/components/_common/index.ts
@@ -0,0 +1,14 @@
+export { BtnConfirm, BtnToggle, IconBtn } from './buttons';
+export { default as Dropdown } from './Dropdown';
+export {
+	CheckBox,
+	InputTextBase,
+	InputTextRange,
+	InputTextSingle,
+	RadioButtonGroup,
+	TextArea,
+} from './inputs';
+export { default as PageHeader } from './PageHeader';
+export { PopupBase, PopupBottom, PopupConfirm } from './popups';
+export { default as Tooltip } from './Tooltip';
+export { SimpleLabel } from './labels';
